2d55c5a788a9c4568975f8eb3cdb2b962c17859a
Juan Pablo Guerra Torres <jp.guerra20@outlook.com>
2020-10-30 12:37:02 -0500

    Sección 2 completada y actualización sección 1

--- Sección 1/Data Import.R
+++ Sección 1/Data Import.R
@@ -1,1 +1,50 @@
+library(dslabs)
+library(tidyverse)    # includes readr
+library(readxl)
+#Ver y cambiar Working Directory
 getwd()
+setwd("HOla")
+
+system.file("extdata",package = "dslabs")
+path <- system.file("extdata", package="dslabs")
+list.files(path)
+
+filename <- "murders.csv"
+fullpath <- file.path(path, filename)
+fullpath
+
+# copy file from dslabs package to your working directory
+file.copy(fullpath, getwd())
+file.exists(filename)
+
+#Leer datos
+
+#readr is the tidyverse library that includes functions for reading data stored in text file spreadsheets into R. Functions in the package include read_csv(), read_tsv(), read_delim() and more. These differ by the delimiter they use to split columns.
+#The readxl package provides functions to read Microsoft Excel formatted files.
+#The excel_sheets() function gives the names of the sheets in the Excel file. These names are passed to the sheet argument for the readxl functions read_excel(), read_xls() and read_xlsx().
+#The read_lines() function shows the first few lines of a file in R.
+
+read_lines("murders.csv", n_max = 3)
+dat <- read_csv(filename)
+
+#read using full path
+dat <- read_csv(fullpath)
+head(dat)
+
+#Usando las funciones básicas de R y no Tidyverse. Todo se convierte en factor. Se usa StragnAsFactor=FALSE
+# filename is defined in the previous video
+# read.csv converts strings to factors
+dat2 <- read.csv(filename)
+class(dat2$abb)
+class(dat2$region)
+
+#Descargar datos de internet. Se crea un archvo temporal y luego de cargar los datos, se elimina
+url <- "https://raw.githubusercontent.com/rafalab/dslabs/master/inst/extdata/murders.csv"
+dat <- read_csv(url)
+download.file(url, "murders.csv")
+tempfile()
+tmp_filename <- tempfile()
+download.file(url, tmp_filename)
+dat <- read_csv(tmp_filename)
+file.remove(tmp_filename)
+
---
+++ Sección 1/Tidy Data.R
@@ -0,0 +1,75 @@
+# import and inspect example of original Gapminder data in wide format
+path <- system.file("extdata", package="dslabs")
+filename <- file.path(path,  "fertility-two-countries-example.csv")
+wide_data <- read_csv(filename)
+select(wide_data, country, `1960`:`1967`)
+head(wide_data)
+
+# gather wide data to make new tidy data
+new_tidy_data <- wide_data %>%
+  gather(year, fertility, `1960`:`2015`)
+head(new_tidy_data)
+
+# gather all columns except country
+new_tidy_data <- wide_data %>%
+  gather(year, fertility, -country)
+
+#Los años quedan como Character porque eran columnas. Se debe corregir
+# convert gathered column names to numeric
+new_tidy_data <- wide_data %>%
+  gather(year, fertility, -country, convert = TRUE)
+class(new_tidy_data$year)
+
+# ggplot works on new tidy data
+new_tidy_data %>%
+  ggplot(aes(year, fertility, color = country)) +
+  geom_point()
+
+# spread tidy data to generate wide data. Revertir proceso de Gather. Primer argumento serán los nombres de las colmnas y el segundo los datos que se usan para rellenar.
+new_wide_data <- new_tidy_data %>% spread(year, fertility)
+select(new_wide_data, country, `1960`:`1967`)
+
+
+# -- EJEMPLO MÁS DENSO --
+
+# import data
+path <- system.file("extdata", package = "dslabs")
+filename <- file.path(path, "life-expectancy-and-fertility-two-countries-example.csv")
+raw_dat <- read_csv(filename)
+select(raw_dat, 1:5)
+
+# gather all columns except country
+dat <- raw_dat %>% gather(key, value, -country)
+head(dat)
+dat$key[1:5]
+
+# separate on underscores
+dat %>% separate(key, c("year", "variable_name"), "_")
+dat %>% separate(key, c("year", "variable_name"))
+
+# split on all underscores, pad empty cells with NA
+dat %>% separate(key, c("year", "first_variable_name", "second_variable_name"), 
+                 fill = "right")
+
+# split on first underscore but keep life_expectancy merged
+dat %>% separate(key, c("year", "variable_name"), sep = "_", extra = "merge")
+
+#APPROCHE 1
+# separate then spread
+dat %>% separate(key, c("year", "variable_name"), sep = "_", extra = "merge") %>%
+  spread(variable_name, value) 
+
+#APPROCHE 2
+# separate then unite
+dat %>% 
+  separate(key, c("year", "first_variable_name", "second_variable_name"), fill = "right") %>%
+  unite(variable_name, first_variable_name, second_variable_name, sep="_")
+
+# full code for tidying data
+dat %>% 
+  separate(key, c("year", "first_variable_name", "second_variable_name"), fill = "right") %>%
+  unite(variable_name, first_variable_name, second_variable_name, sep="_") %>%
+  spread(variable_name, value) %>%
+  rename(fertility = fertility_NA)
+
+

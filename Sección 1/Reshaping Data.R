25e1f4eb9a760352e8cdf20bf38c08aa015d5a0c
Juan Pablo Guerra Torres <jp.guerra20@outlook.com>
2020-10-30 16:04:06 -0500

    Cambiar nombre

--- Sección 1/Reshaping Data.R
+++ Sección 1/Reshaping Data.R
@@ -72,4 +72,20 @@ dat %>%
   spread(variable_name, value) %>%
   rename(fertility = fertility_NA)
 
+#EJERCICIO
+co2
+co2_wide <- data.frame(matrix(co2, ncol = 12, byrow = TRUE)) %>% 
+  setNames(1:12) %>%
+  mutate(year = as.character(1959:1997))
+co2_wide
 
+co2_tidy <- gather(co2_wide,month,co2,-year,convert = TRUE)
+co2_tidy %>% ggplot(aes(as.numeric(month), co2, color = year)) + geom_line()
+
+data(admissions)
+dat <- admissions %>% select(-applicants)
+dat <- spread(dat,gender,admitted)
+admissions
+tmp <- gather(admissions, key, value, admitted:applicants)
+tmp2 <- unite(tmp,column_name,c(key,gender))
+tmp2
